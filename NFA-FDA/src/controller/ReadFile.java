1a1c83a006bbfe414219057f454671c44597bdf0
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-04-18 01:20:13 -0300

    comentários no código - Leitura XML

--- NFA-FDA/src/controller/ReadFile.java
+++ NFA-FDA/src/controller/ReadFile.java
@@ -12,35 +12,50 @@ public class ReadFile {
         this.filePath = filePath;
     }
 
-
-
     public boolean read(){
         String line;
 
-
         try(BufferedReader bf = new BufferedReader(new FileReader(filePath))) {
 
+            //Lê o arquivo até o final do mesmo
             while((line=bf.readLine())!=null){
 
+                //condição para ler os estados
                 if (line.contains("state id") && !line.contains("</state>&#13;")) {
+
+                    /*atributos do objeto estado que serão 
+                    alterados a medida que o arquivo for lido*/
                     int id = 0;
                     String name = "none";
                     String label = "none";
                     Boolean initialState = false;
                     Boolean finalState = false;
+                    //-----------------------------
 
+                    //procura pelo id do estado e altera a variavel id
                     if (line.contains("id=")) id = Integer.parseInt(getSubString(line, "id="));
 
+                    //procura pelo nome do estado e altera a variavel nome
                     if (line.contains("name=")) name = getSubString(line, "name=");
 
+                    //procura pelo label do estado e altera a variavel label
                     if (line.contains("label=")) label = getSubString(line, "label=");
         
+                    /*
+                        Após o programa verificar que existe um estado o loop a seguir
+                        vai verificar se esse estado é final/inicial ou as duas condições
+                    */
                     while ((line=bf.readLine())!=null && !line.contains("</state>&#13;")) {
                         if(line.contains("<initial/>&#13;")) initialState=true;
 
                         if(line.contains("<final/>&#13;")) finalState=true;
                     }
 
+                    /*
+                        ao encontrar a tag de fechamento o metodo 
+                        cria um novo estado e adiciona na lista de estado do automato
+                        Posteriormente, se algum valor foi lido incorretamente será tratado
+                    */
                     if (line.contains("</state>&#13;")) {
                         Estado estado = new Estado(id,name,label,initialState,finalState);
                         System.out.println(estado.toString());
@@ -48,16 +63,12 @@ public class ReadFile {
                     }
                 }
             }
+            return true;
 
-
-                
-
-            } catch (Exception e) {
+        } catch (Exception e) {
             e.getMessage();
             return false;
         }
-
-        return true;
     }
 
     private String getSubString(String line,String lookFor) {
@@ -71,8 +82,8 @@ public class ReadFile {
             }
         }
 
-        if(end==null) end = initial+2;
         //verifica se a busca deu certo, caso contrário ele define que o id possui apenas 1 caractere
+        if(end==null) end = initial+2;
 
         return line.substring(initial+1, end);
     }
