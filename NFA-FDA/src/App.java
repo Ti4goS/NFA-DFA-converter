16512bb7d054a698e5152eb5d0ac92a45d4d4a5b
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-04-18 18:37:37 -0300

    Revert "Criação do menu"

--- NFA-FDA/src/App.java
+++ NFA-FDA/src/App.java
@@ -5,8 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         String path = "D:/Faculdade/5°periodo/Teoria da Computação/Atividades/Atividade-04/EX04Q01.jff";
         ReadFile rf = new ReadFile(path);
-        buildMenu tela = new buildMenu();
+
         rf.read();
-        tela.setVisible(true);
     }
 }
