0f7408bc5e95a42ab3335c5741f0ada324071aee
Luan-S4ntana <95152434+Luan-S4ntana@users.noreply.github.com>
2022-04-21 00:13:10 -0300

    JButton

--- NFA-FDA/src/App.java
+++ NFA-FDA/src/App.java
@@ -1,11 +1,12 @@
 
 import controller.ReadFile;
+import view.BuildMenu;
 
 public class App {
     public static void main(String[] args) throws Exception {
         String path = "D:/Faculdade/5°periodo/Teoria da Computação/Atividades/Atividade-04/EX04Q01.jff";
         ReadFile rf = new ReadFile(path);
-        buildMenu tela = new buildMenu();
+        BuildMenu tela = new BuildMenu();
         rf.read();
         tela.setVisible(true);
     }
--- NFA-FDA/src/view/buildMenu.java
+++ NFA-FDA/src/view/buildMenu.java
@@ -2,12 +2,25 @@ package view;
 
 import javax.swing.*;
 
-public class buildMenu extends JFrame {
-    public buildMenu() {
+public class BuildMenu extends JFrame {
+    JButton botao1 = new JButton("Iniciar conversão");//converte automato
+    JButton botao = new JButton("Selecionar o local do arquivo do AFN");
+    JButton botao2 = new JButton("Salvar AFD");
+
+    public BuildMenu() {
         super("Conversor de Autômatos");
-        final JLabel label = new JLabel("Conversor AFN em AFD");
-        getContentPane().add(label);
+        this.setLocationRelativeTo(null);
+        JPanel painel = new JPanel();
+        botao.setBounds(90, 20, 100, 30);
+        botao1.setBounds(120, 60, 100, 60);
+        botao2.setBounds(150, 100, 100, 90);
+        painel.add(botao);
+        painel.add(botao1);
+        painel.add(botao2);
+        add(painel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
         setVisible(true);
+        setSize(240, 160);
     }
+}
