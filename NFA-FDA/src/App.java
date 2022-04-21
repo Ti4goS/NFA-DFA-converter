c531285610726574126cab1711e2968874775380
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-04-21 15:09:50 -0300

    conversão finalizada

--- NFA-FDA/src/App.java
+++ NFA-FDA/src/App.java
@@ -1,12 +1,14 @@
 import controller.ConverteAutomato;
 import controller.ReadFile;
-import view.buildMenu;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String path = "D:/Faculdade/5°periodo/Teoria da Computação/prova.jff";
+        //D:/Faculdade/5°periodo/Teoria da Computação/prova.jff
+        //String path = "D:/Faculdade/5°periodo/Teoria da Computação/Atividades/Atividade-05/Q01/A-AFN.jff";
+        String path = "D:/Faculdade/5°periodo/Teoria da Computação/Atividades/Atividade-05/Q01/B-AFN.jff";
         ReadFile readFile = new ReadFile(path);
         readFile.read();
+        //System.out.println(ConverteAutomato.afn.toString());
         ConverteAutomato.converter();
     }
 }
